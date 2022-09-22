6a5a8eb400ed9a0445b044ff3fc08a4ac633678b
NoWen22 <nora@norrkross.com>
2022-09-22 11:02:23 +0200

    WallJump
    Har lagt till i scriptet PlayerMovement att spelaren ska kunna få en knuff i y- och x-led när den hoppar mot en vägg. Snyggade även upp scriptet.

--- CherryTale/Assets/Script/PlayerMovement.cs
+++ CherryTale/Assets/Script/PlayerMovement.cs
@@ -20,10 +20,23 @@ public class PlayerMovement : MonoBehaviour {
     public float smoothTime = 0.2f;
     public bool isHurt = false;
     [SerializeField] private LayerMask whatIsGround;
+
+    
     [SerializeField] private AudioSource playerAudioSource;
     [SerializeField] private AudioClip playerJumpAudioClip;
     [SerializeField] private AudioClip playerHurtAudioClip;
     [SerializeField] private AudioClip playerPowerUpEndAudioClip;
+
+
+    [Header("Wall Jump System")]
+    [SerializeField] private LayerMask wallLayer;
+  //  public GameObject wallCheck;
+  
+
+
+
+
+
 
     // Start is called before the first frame update
     public void Start() 
@@ -40,38 +53,33 @@ public class PlayerMovement : MonoBehaviour {
     {
         moveDirection = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) == true)
-        {
+        if ((Input.GetKeyDown(KeyCode.Space) == true)|| (Input.GetKeyDown(KeyCode.W) == true)){
             isJumpPressed = true;
             animator.SetTrigger("DoJump");
-            
         }
-
-        if (Input.GetKeyDown(KeyCode.W) == true)
-        {
-            isJumpPressed = true;
-            animator.SetTrigger("DoJump");
-
-        }
-
-
 
         animator.SetBool("IsGrounded", isGrounded);
         animator.SetFloat("Speed", Mathf.Abs(moveDirection));
         animator.SetBool("IsHurt", isHurt);
+
     }
+
+
+
 
     private void FixedUpdate() 
     {
         isGrounded = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.transform.position, 0.2f, whatIsGround);
+  
+
 
         for (int i = 0; i < colliders.Length; i++) {
-            if (colliders[i].gameObject != gameObject)
-            {
+            if (colliders[i].gameObject != gameObject) {
                 isGrounded = true;
             }
         }
+
 
         Vector3 calculatedMovement = Vector3.zero;            
         float verticalVelocity = 0f;
@@ -80,78 +88,93 @@ public class PlayerMovement : MonoBehaviour {
             verticalVelocity = rigidBody2D.velocity.y;
         }
 
+
         calculatedMovement.x = movementSpeed * 100f * moveDirection * Time.fixedDeltaTime;
         calculatedMovement.y = verticalVelocity;
         Move(calculatedMovement, isJumpPressed);
         isJumpPressed = false;
     }
 
+  
 
-    private void Move(Vector3 moveDirection, bool isJumpPressed)
+
+    private void Move(Vector3 movement, bool isJumpPressed)
     {
-        rigidBody2D.velocity = Vector3.SmoothDamp(rigidBody2D.velocity, moveDirection, ref velocity, smoothTime);
+        //WallJumping börjar här :)
+        //Översättning för nedanför: "Om man hoppar = spelar ljud: om man dessutom hoppar och är på marken = åker uppåt; om man dessutom hoppar och är på en vägg = får en knuff uppåt och åt motsatt direction. Om man är på en vägg men inte hoppar = faller neråt."
 
-        if (isJumpPressed == true && isGrounded == true) {
-            rigidBody2D.AddForce(new Vector2(0f, jumpForce));
+        if (isJumpPressed) {
             playerAudioSource.PlayOneShot(playerJumpAudioClip);
+
+            if (isGrounded) {
+                rigidBody2D.AddForce(new Vector2(0f, jumpForce));
+                
+            } else if (onWall() == true) {
+                rigidBody2D.AddForce(new Vector2(-moveDirection* jumpForce*1.6f, jumpForce*1.4f));
+            }
+
+        } else if (onWall()){
+            rigidBody2D.AddForce(new Vector2(0f, -20f));
         }
 
-        if (moveDirection.x > 0f && isFacingLeft == true) {
+        //WallJumping slutar här :(
+
+        rigidBody2D.velocity = Vector3.SmoothDamp(rigidBody2D.velocity, movement, ref velocity, smoothTime);
+
+
+        if (movement.x > 0f && isFacingLeft == true) {
             FlipSpriteDirection(); 
         }
         
-        else if (moveDirection.x < 0f && isFacingLeft == false) {
+        else if (movement.x < 0f && isFacingLeft == false) {
             FlipSpriteDirection();
         }
     }
+
     
-    private void FlipSpriteDirection()
-    {
+
+    private void FlipSpriteDirection() {
         spriteRenderer.flipX = !isFacingLeft;
         isFacingLeft = !isFacingLeft;
     }
 
-    public bool IsFalling()
-    {
-        if (rigidBody2D.velocity.y < -1f)
-        {
+    public bool IsFalling() {
+        if (rigidBody2D.velocity.y < -1f) {
             return true;
         }
 
         return false;
-
     }
 
 
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy") == true)
-        {
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Enemy") == true) {
             isHurt = true;
             playerAudioSource.PlayOneShot(playerHurtAudioClip);
             Invoke("HurtAnimationEnd", 0.5f);
         }
     }
 
-    public void HurtAnimationEnd()
-    {
+    public void HurtAnimationEnd() {
         isHurt = false;
     }
 
-    public void ResetMovementSpeed()
-    {
+    public void ResetMovementSpeed() {
         movementSpeed = defaultMovementSpeed;
         playerAudioSource.PlayOneShot(playerPowerUpEndAudioClip);
     }
 
-    public void SetNewMovementSpeed(float multiplyBy)
-    {
+    public void SetNewMovementSpeed(float multiplyBy) {
         movementSpeed *= multiplyBy;
         Invoke("ResetMovementSpeed", 5f);
     }
-    
 
 
+    private bool onWall() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(transform.position,
+            Vector3.one, 0, new Vector2(transform.localScale.x, 0), 1f, wallLayer);
+        return raycastHit.collider != null;
+    }
 }
 
 
